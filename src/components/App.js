36e7d20ba6798b7fa608becb4f839e3b26fbeb73
Laura Portillo <lauraportillo.ark@gmail.com>
2021-06-17 10:37:32 +0200

    add react router

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+// React
+import React, { useState } from 'react';
+import { Route, Switch } from 'react-router-dom';
+// Data
+import data from '../data/data.json';
+// Components
+import Header from './Header';
+import Footer from './Footer';
+// Styles
+import '../stylesheets/App.scss';
+import '../stylesheets/Reset.scss';
+
+function App() {
+  return <div></div>;
+}
+
+export default App;
