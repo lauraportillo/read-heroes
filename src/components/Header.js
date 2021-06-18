05bb081c449fa8b7a3d54020bbea477f7e81a454
Laura Portillo <lauraportillo.ark@gmail.com>
2021-06-18 11:48:41 +0200

    add a header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,10 @@ import '../stylesheets/Header.scss';
 
 const Header = (props) => {
   return (
-    <>
-      <div></div>
-    </>
+    <header className="containerHeader">
+      <h1 className="containerHeader__title">Read heroes</h1>
+      <h2 className="containerHeader__subtitle">Your comic bookshop</h2>
+    </header>
   );
 };
 
