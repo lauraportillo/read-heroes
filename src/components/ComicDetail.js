c9d3a3a66a972c31befd22fcba2f1c2d901da7d3
Laura Portillo <lauraportillo.ark@gmail.com>
2021-06-20 23:05:23 +0200

    add responsive design to comic detail component

--- src/components/ComicDetail.js
+++ src/components/ComicDetail.js
@@ -8,11 +8,17 @@ const ComicDetail = (props) => {
       <div className="detail__image">
         <img className="detail__image--photo" src={props.comic.image} alt={props.comic.name} />
       </div>
-      <div>
-        <h2 className="detail__title">{props.comic.name}</h2>
-        <h3 className="detail__subtitle">{props.comic.isbn}</h3>
-        <h3 className="detail__subtitle">{props.comic.author}</h3>
-        <p className="detail__description">{props.comic.description}</p>
+      <div className="detail__info">
+        <h2 className="detail__info--title">{props.comic.name}</h2>
+        <h3 className="detail__info--subtitle">
+          <span className="title">Isbn:</span> {props.comic.isbn}
+        </h3>
+        <h3 className="detail__info--subtitle">
+          <span className="title">Author:</span> {props.comic.author}
+        </h3>
+        <p className="detail__info--description">
+          <span className="title">Description:</span> {props.comic.description}
+        </p>
       </div>
 
       <Link className="detail__btn" to="/">
