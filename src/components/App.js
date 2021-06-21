f3aa76651210ca35615f2b46925ce1c37cecd952
Laura Portillo <lauraportillo.ark@gmail.com>
2021-06-21 10:20:08 +0200

    add nav links working properly

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,8 @@ import { Route, Switch } from 'react-router-dom';
 import data from '../data/data.json';
 // Components
 import Header from './Header';
+import CreateComic from './CreateComic';
+import UpdateComic from './UpdateComic';
 import ComicList from './ComicList';
 import ComicDetail from './ComicDetail';
 import Footer from './Footer';
@@ -35,6 +37,8 @@ function App() {
           <Route path="/" exact>
             <ComicList comics={comics} />
           </Route>
+          <Route path="/create" component={CreateComic} />
+          <Route path="/update" component={UpdateComic} />
           <Route path="/comic/:id" render={renderComicDetail} />
         </Switch>
       </main>
