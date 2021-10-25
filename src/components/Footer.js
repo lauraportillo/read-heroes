97b5cfb59c52e5dd2fc65d0b785c6cbc0eae8c79
Laura Portillo <lauraportillo.ark@gmail.com>
2021-10-25 10:47:32 +0200

    change footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,31 +3,53 @@ import '../stylesheets/Footer.scss';
 
 const Footer = (props) => {
   return (
-    <footer className="containerFooter">
-      <div className="containerFooter__icons">
-        <a className="containerFooter__icons--link" href="https://twitter.com/LauraPo02860847" target="_blank">
-          <i className="fa fa-twitter" alt="twitter icon"></i>
-        </a>
-        <a
-          className="containerFooter__icons--link"
-          href="https://www.linkedin.com/in/laura-portillo-rodr%C3%ADguez-21965a86/"
-          target="_blank"
-        >
-          <i className="fa fa-linkedin" alt="linkedin icon"></i>
-        </a>
-
-        <a className="containerFooter__icons--link" href="https://github.com/lauraportillo" target="_blank">
-          <i className="fa fa-github-alt" alt="github icon"></i>
-        </a>
-
-        <a className="containerFooter__icons--link" href="mailto:lauraportillo.ark@gmail.com" target="_blank">
-          <i className="fa fa-envelope" alt="envelope icon"></i>
-        </a>
-      </div>
-      <div className="containerFooter__copy">
-        <small>By Laura Portillo Rodríguez</small>
-        <small>©2021</small>
-      </div>
+    <footer>
+      <ul className="footer">
+        <li className="footer__item">
+          <a
+            className="footer__item--icon"
+            href="https://www.linkedin.com/in/laura-portillo-rodr%C3%ADguez-21965a86/"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="linkedin"
+          >
+            <i className="fa fa-linkedin" alt="linkedin icon"></i>
+          </a>
+        </li>
+        <li className="footer__item">
+          <a
+            className="footer__item--icon"
+            href="https://github.com/lauraportillo"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="github"
+          >
+            <i className="fa fa-github-alt" alt="github icon"></i>
+          </a>
+        </li>
+        <li className="footer__item">
+          <a
+            className="footer__item--icon"
+            href="https://twitter.com/LauraPo02860847"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="twitter"
+          >
+            <i className="fa fa-twitter" alt="twitter icon"></i>
+          </a>
+        </li>
+        <li className="footer__item">
+          <a
+            className="footer__item--icon"
+            href="mailto:lauraportillo.ark@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="email"
+          >
+            <i className="fa fa-envelope" alt="envelope icon"></i>
+          </a>
+        </li>
+      </ul>
     </footer>
   );
 };
